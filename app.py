4a8befeac19c204b89aee5255a288ba6c434c427
Sumit K Kumawat <31368296+SumitKKumawat@users.noreply.github.com>
2021-05-30 16:42:25 +0530

    app

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from requests import sessions
 proxy = {
     "https": "https://14.140.131.82:3128",
     "http": "https://14.140.131.82:3128"}
-
-
 #proxy = {
 #    "https": "14.140.131.82:3128",
 #    "http": "14.140.131.82:3128"}
@@ -20,24 +18,23 @@ browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit
 
 def main():
     while(1):
-        #if datetime.datetime.now().hour==9 and datetime.datetime.now().minute==1 and datetime.datetime.now().second>1 and datetime.datetime.now().second<5:
-        welcome="https://api.telegram.org/bot1825518407:AAGvVNzW1QgLmlJ8fRuSCl1yuM63q7PBgx0/sendMessage?chat_id=-1001288829686&text=Welcome, Developed by:- Sumit K Kumawat"
-        requests.get(welcome)
+        if datetime.datetime.now().hour==9 and datetime.datetime.now().minute==1 and datetime.datetime.now().second>1 and datetime.datetime.now().second<5:
+            welcome="https://api.telegram.org/bot1825518407:AAGvVNzW1QgLmlJ8fRuSCl1yuM63q7PBgx0/sendMessage?chat_id=-1001288829686&text=Welcome, Developed by:- Sumit K Kumawat"
+            requests.get(welcome)
         for i in range(532,533):
             i=str(i)
             x="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+i+"&date=" + datetime.date.today().strftime('%d-%m-%Y')   #"29-05-2021" datetime.date.today().strftime('%d-%m-%Y')
             data=requests.get(x,headers=browser_header,proxies=proxy)
+            #data=requests.get(x,headers=browser_header)
             results=json.loads(data.text)
             print(x)
-            #print(results)
             print("-----------------------------------------------")
             count=results["sessions"]
-            #count=results
             if len(count)>0:
                 for session in count:
                     msg=[]
                     msg.append({"district_name":session["district_name"],"centre_name":session["name"],"centre_address":session["address"],"vaccine":session["vaccine"],"fee":session["fee"],"availability":session["available_capacity"],"minage":session["min_age_limit"],"date":session["date"],"slots":session["slots"]})
-                    if session["available_capacity"]!=0:
+                    if session["available_capacity"]!=0 and ("MINDA" in session["name"].upper() or "DEVLI KALLAN" in session["name"].upper() or "NAWA" in session["name"].upper() or "MAROTH" in session["name"].upper()):
                         parse_data=json.dumps(msg)
                         parse_data=parse_data.replace("{","")
                         parse_data=parse_data.replace("}","\n\n")
