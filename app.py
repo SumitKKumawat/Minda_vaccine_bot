5c44296c81e7b0c1381a2558da3352db0d18ad8a
Sumit K Kumawat <31368296+SumitKKumawat@users.noreply.github.com>
2021-05-30 07:29:35 +0530

    Telegram bot

---
+++ app.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import time
+import datetime
+from requests import sessions
+
+# faking chrome browser
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+while(1):
+    if datetime.datetime.now().hour==9 and datetime.datetime.now().minute==1 and datetime.datetime.now().second>1 and datetime.datetime.now().second<5:
+        welcome="https://api.telegram.org/bot1825518407:AAGvVNzW1QgLmlJ8fRuSCl1yuM63q7PBgx0/sendMessage?chat_id=-1001288829686&text=Welcome, Developed by:- Sumit K Kumawat"
+        requests.get(welcome)
+    for i in range(532,533):
+        i=str(i)
+        x="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+i+"&date=" + datetime.date.today().strftime('%d-%m-%Y')   #"29-05-2021" datetime.date.today().strftime('%d-%m-%Y')
+        data=requests.get(x,headers=headers)
+        results=json.loads(data.text)
+        count=results["sessions"]
+        if len(count)>0:
+            for session in count:
+                msg=[]
+                msg.append({"district_name":session["district_name"],"centre_name":session["name"],"centre_address":session["address"],"vaccine":session["vaccine"],"fee":session["fee"],"availability":session["available_capacity"],"minage":session["min_age_limit"],"date":session["date"],"slots":session["slots"]})
+                if session["available_capacity"]!=0:
+                    parse_data=json.dumps(msg)
+                    parse_data=parse_data.replace("{","")
+                    parse_data=parse_data.replace("}","\n\n")
+                    parse_data=parse_data.replace("[","")
+                    parse_data=parse_data.replace("]","")
+                    parse_data=parse_data.replace(",","\n")
+                    nd_url="https://api.telegram.org/bot1825518407:AAGvVNzW1QgLmlJ8fRuSCl1yuM63q7PBgx0/sendMessage?chat_id=-1001288829686&text= "+parse_data
+                    requests.get(nd_url)
+                    y=requests.get(nd_url)
+                    #time.sleep(50)
+    print("while loop")
+    #time.sleep(4)
+
+
+
+
