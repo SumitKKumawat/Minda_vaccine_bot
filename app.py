fcb037509cd2bb8183fe79345ca47493ccc65396
Sumit K Kumawat <31368296+SumitKKumawat@users.noreply.github.com>
2021-05-30 17:56:05 +0530

    app

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def main():
     while(1):
         if datetime.datetime.now().hour==9 and datetime.datetime.now().minute==1 and datetime.datetime.now().second>1 and datetime.datetime.now().second<5:
             welcome="https://api.telegram.org/bot1825518407:AAGvVNzW1QgLmlJ8fRuSCl1yuM63q7PBgx0/sendMessage?chat_id=-1001288829686&text=Welcome, Developed by:- Sumit K Kumawat"
-            requests.get(welcome,proxies=proxy)
+            requests.get(welcome)
         for i in range(532,533):
             i=str(i)    
             x="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+i+"&date=" + datetime.date.today().strftime('%d-%m-%Y')   #"29-05-2021" datetime.date.today().strftime('%d-%m-%Y')
@@ -42,7 +42,7 @@ def main():
                         parse_data=parse_data.replace("]","")
                         parse_data=parse_data.replace(",","\n")
                         nd_url="https://api.telegram.org/bot1825518407:AAGvVNzW1QgLmlJ8fRuSCl1yuM63q7PBgx0/sendMessage?chat_id=-1001288829686&text= "+parse_data
-                        requests.get(nd_url,proxies=proxy)
+                        requests.get(nd_url)
                         parse_data=""
                         #y=requests.get(nd_url)
                         #time.sleep(50)
